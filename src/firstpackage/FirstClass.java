26afe59fd48940c5443074007ba6c876bc2536fe
Gunesahin <mahirihsanazra@gmail.com>
2020-06-20 20:35:11 +0200

    ikinci commit local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit from local repository");
+		System.out.println("second commint from local repository");
 
 	}
 
